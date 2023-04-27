29391fa6ef70dd51e8f931fa30943194d31c4f67
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-26 22:11:51 -0600

    Feature/avatar component (#6)
    * WIP
    
    * Built Avatar Component
    
    * Updated avatar story image

---
+++ components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,40 @@
+import { Avatar } from '@/components/Avatar';
+import type { Meta, StoryObj } from '@storybook/react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof Avatar> = {
+  title: 'Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const IconAvatar: Story = {
+  args: {
+    size: 'lg',
+    style: 'square',
+    variant: 'icon',
+    image: "https://picsum.photos/seed/virtual-pantry/200"
+  },
+};
+
+export const ImageAvatar: Story = {
+  args: {
+    size: 'lg',
+    style: 'square',
+    variant: 'image',
+    image: 'https://picsum.photos/seed/virtual-pantry/200'
+  },
+};
+
+export const InitialsAvatar: Story = {
+  args: {
+    size: 'lg',
+    style: 'square',
+    variant: 'initials',
+    initials: 'AB'
+  },
+};
