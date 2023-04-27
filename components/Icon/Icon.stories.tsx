03e2993c91bfae931515e49ef5ea4aae70662548
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-26 21:00:52 -0600

    Built Icon Component (#5)

--- components/Icon/Icon.stories.tsx
+++ components/Icon/Icon.stories.tsx
@@ -1,20 +1,21 @@
-import { TestComponent } from '@/components/TestComponent';
+import { Icon } from '@/components/Icon';
 import type { Meta, StoryObj } from '@storybook/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
-const meta: Meta<typeof TestComponent> = {
-  title: 'TestComponent',
-  component: TestComponent,
+const meta: Meta<typeof Icon> = {
+  title: 'Icon',
+  component: Icon,
   tags: ['autodocs'],
   argTypes: {},
 };
 
 export default meta;
-type Story = StoryObj<typeof TestComponent>;
+type Story = StoryObj<typeof Icon>;
 
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const Primary: Story = {
+export const UserCircle: Story = {
   args: {
-    children: 'Lorem ipsum',
+    size: 'lg',
+    type: 'userCircle',
   },
 };
