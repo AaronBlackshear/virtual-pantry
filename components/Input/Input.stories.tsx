3cb0a9939cb635ff2b6fb13bc535d2321dde8930
Aaron Blackshear <ablackshear7820@gmail.com>
2023-05-04 11:45:32 -0600

    Built Input component

--- components/Input/Input.stories.tsx
+++ components/Input/Input.stories.tsx
@@ -1,7 +1,6 @@
 import { Input } from '@/components/Input';
 import type { Meta, StoryObj } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Input> = {
   title: 'Form/Input',
   component: Input,
@@ -20,7 +19,6 @@ const meta: Meta<typeof Input> = {
 export default meta;
 type Story = StoryObj<typeof Input>;
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
 export const Primary: Story = {
   args: {
     label: "First name"
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -14,14 +14,20 @@ type Props = {
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 export function Input({ label, type = 'text', helperText, state, iconLeft, iconRight, fullWidth = false, ...props }: Props) {
+  const sharedIconClasses = classNames(
+    "absolute top-4 text-gray-1 peer-disabled:text-gray-10",
+    state === 'success' && "text-green-1",
+    state === 'error' && "text-red-3",
+  )
+
   return (
     <div className={classNames(
       "flex flex-col items-start space-y-2 flex-1",
       fullWidth ? "max-w-none" : "max-w-[320px]",
     )}>
-      <label className="relative w-full">
-        <input type={type} placeholder={label} {...props} aria-invalid="true" className={classNames(
-          "peer h-14 button-md w-full text-gray-2 px-4 pt-7 pb-2 bg-white border rounded-2xl overflow-hidden placeholder-transparent transition-all appearance-none",
+      <label className="relative w-full flex items-center">
+        <input {...props} type={type} placeholder={label} className={classNames(
+          "peer h-14 button-md w-full text-gray-2 px-4 pt-6 pb-1.5 bg-white border rounded-2xl overflow-hidden placeholder-transparent transition-all appearance-none",
           "border-[#f1f1f1] focus:border-blue-3 focus:outline outline-blue-3 outline-1",
           "hover:border-blue-3",
           !!iconLeft ? 'pl-12' : 'pl-4',
@@ -30,18 +36,23 @@ export function Input({ label, type = 'text', helperText, state, iconLeft, iconR
           state === 'error' && "text-red-3 border-red-3",
           "disabled:bg-gray-12 disabled:text-gray-8 disabled:border-[#f1f1f1]"
         )} />
+
         <p className={classNames(
-          "absolute body-small text-gray-8 top-1.5 transition-all",
-          "peer-placeholder-shown:top-4 peer-focus:top-1.5",
+          "absolute body-small text-gray-8 top-2.5 transition-all",
+          "peer-placeholder-shown:top-5 peer-focus:top-2.5",
           !!iconLeft ? 'left-12' : 'left-4',
-        )}>{label}</p>
+        )}>
+          {label}
+        </p>
+
         {iconLeft && (
-          <div className="absolute top-3 left-2">
+          <div className={`${sharedIconClasses} left-3`}>
             <Icon type={iconLeft} size="md" />
           </div>
         )}
+
         {iconRight && (
-          <div className="absolute top-3 right-2">
+          <div className={`${sharedIconClasses} right-3`}>
             <Icon type={iconRight} size="md" />
           </div>
         )}
