7991f993128393cb187d3e74530a13bbc65ba49e
Aaron Blackshear <ablackshear7820@gmail.com>
2023-05-04 10:34:58 -0600

    Overhaul meal plan schema models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-
 async function main() {
   const user = await prisma.user.create({
     data: {
@@ -19,19 +18,31 @@ async function main() {
       startDate: new Date('2023-05-02'),
       endDate: new Date('2023-05-09'),
       userId: user.id,
-      meals: {
+      mealPlanItems: {
         create: [
           {
-            title: 'Protein shake',
-            userId: user.id,
+            itemDate: new Date(),
+            meal: {
+              create: {
+                name: 'Banana smoothie',
+                userId: user.id,
+              }
+            },
+            mealCategory: {
+              create: {
+                name: 'Breakfast',
+                theme: 'gray',
+                order: 1,
+                userId: user.id,
+              }
+            }
           },
           {
-            userId: user.id,
-            mealCategory: 'Dinner',
+            itemDate: new Date(),
             recipe: {
               create: {
                 userId: user.id,
-                title: "Chicken Fried Steak with Mashed Potatoes",
+                name: "Chicken Fried Steak with Mashed Potatoes",
                 servings: 2,
                 prepTime: 15,
                 cookTime: 45,
@@ -45,24 +56,26 @@ async function main() {
                 ingredients: {
                   create: {
                     amount: 2,
+                    ingredientMeasurement: "cups",
                     ingredient: {
                       create: {
                         name: 'Flour',
                         userId: user.id,
                       }
                     },
-                    ingredientMeasurement: {
-                      create: {
-                        singularMeasurement: 'cup',
-                        pluralMeasurement: 'cups',
-                        userId: user.id,
-                      }
-                    }
                   }
                 }
               }
+            },
+            mealCategory: {
+              create: {
+                name: 'Dinner',
+                theme: 'gray',
+                order: 1,
+                userId: user.id,
+              }
             }
-          }
+          },
         ]
       }
     },
